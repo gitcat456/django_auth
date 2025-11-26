90f92e568de454b3674fe3c867b56f5ee497d043
gitcat456 <okothdenzel65@gmail.com>
2025-11-27 00:22:41 +0300

    imports the custom form

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.views.generic import CreateView, DetailView
-from django.contrib.auth.forms import UserCreationForm 
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin 
+from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     template_name = 'accounts/signup.html'
     success_url = reverse_lazy('login')
     
