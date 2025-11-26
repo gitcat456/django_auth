e1959242aac4af5a5a24ef1822041b4b6a040f00
gitcat456 <okothdenzel65@gmail.com>
2025-11-26 23:39:21 +0300

    adds a protected user profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import SignUpView
+from .views import SignUpView, UserProfile
 
 urlpatterns = [
-    path('signup', SignUpView.as_view(), name ='signup'),
-    path('login', LoginView.as_view(template_name='accounts/login.html'), name ='login'),
-    path('logout', LogoutView.as_view(template_name='accounts/logout.html'), name ='logout'),
+    path('', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('profile/', UserProfile.as_view(), name='user_profile'),
 ]
